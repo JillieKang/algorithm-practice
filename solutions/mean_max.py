355a434be7a8677df2f5e89dd30e8bd1aa9e0ffd
Jillie <19860415z@gmail.com>
2026-06-12 10:15:42 +0900

    Create mean_max.py

---
+++ solutions/mean_max.py
@@ -0,0 +1,15 @@
+T = int(input())
+ 
+for test_case in range(1, T + 1):
+    N = int(input())
+    ai = list(map(int, input().split()))
+ 
+    max_v = ai[0]
+    min_v = ai[0]
+    for i in range(N):
+        if max_v < ai[i]:
+            max_v = ai[i]
+        if min_v > ai[i]:
+            min_v = ai[i]
+ 
+    print(f'#{test_case} {max_v - min_v}')
