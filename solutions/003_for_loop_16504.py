8461ec6acd587174e9172d181f14f1fa1d7a70cf
Jillie <19860415z@gmail.com>
2026-06-12 10:45:05 +0900

    Update 003_for_loop_16504.py

--- solutions/003_for_loop_16504.py
+++ solutions/003_for_loop_16504.py
@@ -12,7 +12,7 @@ for test_case in range(1, T+1):
             if arr[i] > arr[j]:  # i번째 건물이 j번째 건물보다 크다면
                 now_fall += 1    # i번째 건물과 j번째 건물을 비교할 때마다 += 업데이트 여부 결정
  
-        if max_fall < now_fall:     # i번째 건물에 대해서 N-1번째 건물까지 다 돌아서 now_fall을 업데이트 완료 후
+        if max_fall < now_fall:     # i번째 건물에 대해서 N-1번째 건물까지 다 돌아서 now_fall을 업데이트 한 다음
             max_fall = now_fall     # 만약 이 i번째 건물의 최종 now_fall이 max_fall보다 크다면 max_fall을 업데이트
                                     # max_fall은 i번째 건물의 now_fall이 완성 될 때마다 비교 후 업데이트 여부 결정
  
