2eb8265d9bfebb91e4b3277ce44c1fed60e8cd16
Jillie <19860415z@gmail.com>
2026-06-12 11:29:48 +0900

    Update 010_character_frequency.py

--- solutions/010_character_frequency.py
+++ solutions/010_character_frequency.py
@@ -12,13 +12,13 @@ for test_case in range(1, T+1):
     str2 = input()
  
     # str1 속 글자가 str2 속에 있는지 검사
-    # str1 속 해당 글자가 str2 속에 있을 때마다 1씩 더함(+)
-    # str1의 길이만큼 빈 리스트를 만들어서 올려주고
-    # 가장 큰 값의 인덱스를 이용하자
+    # str1의 길이만큼 빈 리스트를 만들어서
+    # str1 속 해당 글자가 str2 속에 있을 때마다 1씩 count(+=)한 다음
+    # 가장 큰 값의 인덱스를 이용
     count = [0]*len(str1)
-    # 일단 str1 중 하나를 골라
+    # str1 중 하나를 골라
     for i in range(len(str1)):
-        # 그리고 그 i를 str2 속 j들과 일일이 대조하자
+        # i를 str2 속 j들과 일일이 대조하자
         for j in range(len(str2)):
             if str1[i] == str2[j]:
                 count[i] += 1
