ee8b142d7da8513719bd1ea6afd1dc72f62ec8e6
Jillie <19860415z@gmail.com>
2026-06-12 11:02:05 +0900

    Update 001_max_min_difference.py

--- solutions/001_max_min_difference.py
+++ solutions/001_max_min_difference.py
@@ -7,15 +7,16 @@ Topic: Max-min difference
 
 T = int(input())
  
-for test_case in range(1, T+1):
+for test_case in range(1, T + 1):
     N = int(input())
     ai = list(map(int, input().split()))
  
-    max_sum = 0
-    sum_now = 0
-    for i in range(N-1):
-        sum_now = ai[i] + ai[i+1]
-        if max_sum < sum_now:
-            max_sum = sum_now
+    max_v = ai[0]
+    min_v = ai[0]
+    for i in range(N):
+        if max_v < ai[i]:
+            max_v = ai[i]
+        if min_v > ai[i]:
+            min_v = ai[i]
  
-    print(f'#{test_case} {max_sum}')
+    print(f'#{test_case} {max_v - min_v}')
